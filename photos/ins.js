1bbf59db178db17bfbd1dd64769b5324899d8d98
gengjiayao <1098469310@qq.com>
2024-03-12 18:37:53 +0800

    Site updated: 2024-03-12 18:37:53

--- photos/ins.js
+++ photos/ins.js
@@ -125,8 +125,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = "https://img-date.oss-cn-hangzhou.aliyuncs.com/min_photos/" + data.link[i];
-            var src = "https://img-date.oss-cn-hangzhou.aliyuncs.com/photos/" + data.link[i];
+            var minSrc = "https://github.com/gengjiayao/hexo/tree/main/min_photos/" + data.link[i];
+            var src = "https://github.com/gengjiayao/hexo/tree/main/photos/" + data.link[i];
             var type = data.type[i];
             var target = src + (type === "video" ? ".mp4" : ".jpg");
             src += "";
