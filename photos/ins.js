feff0d02edc31cca74b5ed4abe3356ea1177c1e3
gengjiayao <1098469310@qq.com>
2024-03-12 18:54:49 +0800

    Site updated: 2024-03-12 18:54:49

--- photos/ins.js
+++ photos/ins.js
@@ -125,8 +125,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = "https://github.com/gengjiayao/hexo/tree/main/min_photos/" + data.link[i];
-            var src = "https://github.com/gengjiayao/hexo/tree/main/photos/" + data.link[i];
+            var minSrc = "./min_photos/" + data.link[i];
+            var src = "./photos/" + data.link[i];
             var type = data.type[i];
             var target = src + (type === "video" ? ".mp4" : ".jpg");
             src += "";
